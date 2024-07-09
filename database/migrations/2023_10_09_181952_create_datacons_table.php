a1f92994e780a42ff92f028f5f1a620bc4b96d01
koi <sunisaixk@gmail.com>
2024-07-09 12:33:04 +0700

    feat : ตัวเชื่อม

--- database/migrations/2023_10_09_181952_create_datacons_table.php
+++ database/migrations/2023_10_09_181952_create_datacons_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->unsignedBigInteger('category_id');
             $table->timestamps();
 
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
